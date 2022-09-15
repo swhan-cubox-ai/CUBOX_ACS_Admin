28ecaf6e1e82354c793f980026606cbd829d096b
sunu <sunu.develop@gmail.com>
2022-09-15 16:46:13 +0900

    출입문관리
    DoorController 생성 및 관련 메서드 생성
    DoorService,impl, dao, xml 드래프트 버전 생성

---
+++ src/main/java/aero/cubox/door/service/impl/DoorDAO.java
@@ -0,0 +1,44 @@
+package aero.cubox.door.service.impl;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository("doorDAO")
+public class DoorDAO extends EgovAbstractMapper {
+
+	private static final String sqlNameSpace = "door.";
+
+
+	public List<Map> getDoorList(Map<String, Object> commandMap) {
+		return selectList(sqlNameSpace+"selectDoorList", commandMap);
+	}
+
+	public Map getDoorInformation(Map<String, Object> commandMap) {
+		return selectOne(sqlNameSpace+"selectDoorInformation", commandMap);
+	}
+
+	public List<HashMap> getPermissionGroups(Map<String, Object> commandMap) {
+		return selectList(sqlNameSpace+"selectPermissionGroup", commandMap);
+	}
+
+
+	public List<HashMap> searchTerminalInformation(Map<String, Object> commandMap) {
+		return selectList(sqlNameSpace+"selectTerminalInformation", commandMap);
+	}
+
+	public void deleteDoor(Map<String, Object> commandMap) {
+		delete(sqlNameSpace+"deleteDoor", commandMap);
+	}
+
+	public void updateDoorInformation(Map<String, Object> commandMap) {
+		update(sqlNameSpace+"updateDoorInformation", commandMap);
+	}
+
+	public HashMap getTerminalInformation(Map<String, Object> commandMap) {
+		return selectOne(sqlNameSpace+"selectTerminalInformation", commandMap);
+	}
+}
